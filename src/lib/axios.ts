8f6c1cc4e2f37273a53c492bae2369c7f97c540b
中村　迅 <shu.kyu0224@icloud.com>
2022-01-02 07:20:29 +0900

    URL変更

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,6 +2,7 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 
-axios.defaults.baseURL = 'http://localhost:8000/api';
+// axios.defaults.baseURL = 'http://localhost:8000/api';
+axios.defaults.baseURL = 'https://jwt-axios-test.herokuapp.com/api';
 
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + Cookies.get('_myapp_token')
