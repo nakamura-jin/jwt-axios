f2acdbf2574f147bf35601a418593099da8ec8b8
中村　迅 <shu.kyu0224@icloud.com>
2022-01-02 04:27:06 +0900

    URL編集

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default axios.create({
   // baseURL: 'http://localhost:8000/api',
-  baseURL: 'https://jwt-axios-test.herokuapp.com/api/login',
+  baseURL: 'https://jwt-axios-test.herokuapp.com/api',
 
   // headers: {
   //   'Content-Type': 'application/json',
