67523e13233e64ecb6de4aca3859a88b3d0ee4fd
中村　迅 <shu.kyu0224@icloud.com>
2022-01-01 18:36:34 +0900

    first commit

---
+++ src/services/middleware.ts
@@ -0,0 +1,12 @@
+import Cookies from 'js-cookie'
+
+export default {
+  auth(to: any, from: any , next: any) {
+    const token = Cookies.get('_myapp_token');
+    if (!token) {
+      return next('/login');
+    }
+
+    next();
+  }
+}
---
+++ src/views/Login.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-row justify="center" align-content="center" class="mt-12">
+    <v-col cols="4">
+      <v-card>
+        <v-app-bar color="primary" dark>
+          <v-card-title>Login</v-card-title>
+        </v-app-bar>
+        <v-col class="pa-8">
+          <v-text-field label="Email" v-model="email" dense></v-text-field>
+          <v-text-field label="Password" v-model="password" dense></v-text-field>
+          <p v-if="error">{{ error }}</p>
+        </v-col>
+        <v-col class="text-right">
+          <v-btn color="primary" @click="submit">Login</v-btn>
+        </v-col>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api'
+import Cookies from 'js-cookie'
+import axios from 'axios'
+
+export default defineComponent({
+  data() {
+    return {
+      email: '',
+      password: '',
+      error: '' as string | unknown
+    }
+  },
+  methods: {
+    submit() {
+      const payload = {
+        email: this.email,
+        password: this.password
+      };
+      // fetch(`http://localhost:8000/api/login`, {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     'Access': 'application/json'
+      //   },
+      //   body: JSON.stringify(payload)
+      // })
+      // .then((response) => response.json())
+      // .then(res => {
+      //   Cookies.set('_myapp_token', res.access_token)
+      //   console.log(res)
+      // })
+      axios({
+        method : 'POST',
+        // url    : 'http://localhost:8000/api/login',
+        url    : 'https://jwt-axios-test.herokuapp.com//api/login',
+        data   : { email: this.email, password: this.password },
+        headers: {
+          'Content-Type': 'application/json',
+          'Access': 'application/json'
+        },
+      })
+      .then(response => Cookies.set('_myapp_token', response.data.access_token))
+    }
+  }
+})
+</script>
