a866a6e9d96449ec65dedec8ec66f59ccccec27b
中村　迅 <shu.kyu0224@icloud.com>
2022-01-12 11:57:08 +0900

    endpoint

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,8 +3,8 @@ import Cookies from 'js-cookie'
 
 
 // axios.defaults.baseURL = 'http://localhost:8000/api';
-axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
-// axios.defaults.baseURL = 'https://jwt-axios-test.herokuapp.com/api';
+// axios.defaults.baseURL = process.env.VUE_APP_API_BASE_URL;
+axios.defaults.baseURL = 'https://jwt-axios-test.herokuapp.com/api';
 
 
 axios.defaults.headers.common['Authorization'] = 'Bearer ' + Cookies.get('_myapp_token')
